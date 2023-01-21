44cee5f490c8792922d79b74e8cfc6af810000c1
CyberMan-00 <miran.shamaev@gmail.com>
2023-01-21 17:38:26 +0100

    forms validation added

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    // slick slider 
     $('.carousel__inner').slick({
         speed: 1500,
         infinite: true,
@@ -9,13 +10,13 @@ $(document).ready(function(){
         prevArrow: '<button type="button" class="slick-prev"><img src="../icons/chevron-left-solid.png" alt="prevArrow"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="../icons/chevron-right-solid.png" alt="nextArrow"></button>'
     });
-
+    // catalog - tabs switch 
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
         $(this)
             .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
             .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
     });
-
+    // catalog - items descr switch
     function toggleSlide(item) {
         $(item).each(function(i) {
             $(this).on('click', function(e) {
@@ -28,6 +29,7 @@ $(document).ready(function(){
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
 
+    // modal windows
     $('[data-modal=consultation]').on('click', function() {
         $('.overlay, #consultaion').fadeIn("slow");
     });
@@ -43,4 +45,48 @@ $(document).ready(function(){
         e.preventDefault();
         $('.overlay, #consultaion, #thanks, #order').fadeOut("slow");
     });
+
+    // validating forms - simple method
+    // $('#consultaion-form').validate();
+    // $('#order form').validate();
+    
+    // validating using own function
+    function validateForms(form) {
+        $(form).validate({
+            // simple rule
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2 // min number of characters allowed
+                },
+                phone: {
+                    required: true
+                },
+                // compound rule
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            // custom messages
+            messages: {
+                name: {
+                    required: "Specify your name, please",
+                    minlength: jQuery.validator.format("At least {0} characters required!")
+                },
+                phone: "We need your phone number to contact you",
+                email: {
+                    required: "We need your email address to contact you",
+                    email: "Your email address must be in the format of example@example.com"
+                }
+            }
+        });
+    }
+
+    validateForms('#consultaion-form');
+    validateForms('#consultaion form');
+    validateForms('#order form');
+
+    // phone mask 
+    $('input[name=phone]').mask("+48-999-999-999");
 });
